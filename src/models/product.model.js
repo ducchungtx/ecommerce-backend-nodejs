f3c7a13d421cf1139c93475ccc4ab77108c1c1cd
Chung Nguyen <zrmedia9@gmail.com>
2023-05-01 11:08:19 +0700

    Done 13

---
+++ src/models/product.model.js
@@ -0,0 +1,92 @@
+'use strict';
+const { model, Schema } = require('mongoose'); // Erase if already required
+
+const DOCUMENT_NAME = 'Product';
+const COLLECTION_NAME = 'Products';
+
+// Declare the Schema of the Mongo model
+var userSchema = new mongoose.Schema(
+  {
+    product_name: {
+      type: String,
+      trim: true,
+      maxLength: 150,
+    },
+    product_thumb: {
+      type: String,
+      unique: true,
+      trim: true,
+    },
+    product_description: {
+      type: String,
+    },
+    product_price: {
+      type: Number,
+      required: true,
+    },
+    product_quality: {
+      type: Number,
+      required: true,
+    },
+    product_type: {
+      type: String,
+      required: true,
+      enum: ['Electronics', 'Clothing', 'Furniture'],
+    },
+    product_shop: {
+      type: Schema.Types.ObjectId,
+      ref: 'Shop',
+    },
+    product_attributes: {
+      type: Schema.Types.Mixed,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    collection: COLLECTION_NAME,
+  }
+);
+
+const electronicsSchema = new Schema(
+  {
+    manufacturer: { type: String, required: true },
+    model: String,
+    color: String,
+  },
+  {
+    collection: 'electronics',
+    timestamps: true,
+  }
+);
+
+const clothingSchema = new Schema(
+  {
+    brand: { type: String, required: true },
+    size: String,
+    material: String,
+  },
+  {
+    collection: 'clothes',
+    timestamps: true,
+  }
+);
+
+const furnitureSchema = new Schema(
+  {
+    brand: { type: String, required: true },
+    size: String,
+    material: String,
+  },
+  {
+    collection: 'furniture',
+    timestamps: true,
+  }
+);
+//Export the model
+module.exports = {
+  product: model(DOCUMENT_NAME, productSchema),
+  electronic: model('Electronics', electronicsSchema),
+  clothing: model('Clothing', clothingSchema),
+  furniture: model('Furniture', furnitureSchema),
+};
